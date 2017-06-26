111906b2d104bf652ead02d6ad0c92c62702f86f
Assen Kolov <assen.kolov@gmail.com>
2017-06-26 22:28:02 +0200

    readme

--- README.md
+++ README.md
@@ -5,7 +5,12 @@
     npm install
     bower install
     gulp build
+    
+## To run
+
     gulp serve
+    
+Edit `server.js` to point to a running backend somewhere.
     
 ## To deploy
 
--- gulp/server.js
+++ gulp/server.js
@@ -33,11 +33,19 @@ function browserSyncInit(baseDir, browser) {
    *
    * For more details and option, https://github.com/chimurai/http-proxy-middleware/blob/v0.9.0/README.md
    */
-  server.middleware = proxyMiddleware('/service/users',
+  server.middleware = [
+    proxyMiddleware('/login',
     {
-      target: 'http://localhost:8501/',
+      target: 'http://localhost:8080/',
       changeOrigin: true
-    });
+    }),
+   proxyMiddleware('/service',
+    {
+      target: 'http://localhost:8080/',
+      changeOrigin: true
+    })
+  ]
+  ;
 
   browserSync.instance = browserSync.init({
     startPath: '/',
